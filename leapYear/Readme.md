f7482be24ce85d5962ecf8c03412562564f808ff
Kshitij Jadhav <kshitij.jadhav@cctech.co.in>
2022-11-25 07:33:11 +0530

    added readme for isLeapYear.

---
+++ leapYear/Readme.md
@@ -0,0 +1,7 @@
+Task : Is the given year a leap year or not
+Test Cases
+- test 1 = if 4 then result true
+- test 2 = if 100 then result false
+- test 3 = if 400 then result true
+- test 4 = if 2000 then result true
+- test 5 = if 2002 then result false
--- leapYear/testLeapYear.cpp
+++ leapYear/testLeapYear.cpp
@@ -1,34 +1,14 @@
 #include <iostream>
 #include "isLeapYear.h"
+#include <cassert>
 bool test();
 
 
 int main (void){
-    if(!test()){
-        std :: cerr << "test failed" << std :: endl;
-        return EXIT_FAILURE;
-    }
-    
+    assert(isLeapYear(4));
+    assert(!isLeapYear(100));
+    assert(isLeapYear(400));
+    assert(isLeapYear(2000));
+    assert(!isLeapYear(2002)); 
     return EXIT_SUCCESS;
 }
-
-bool test(){
-    if(!isLeapYear(2000)){
-        return false;
-    }
-    if(isLeapYear(2002)){
-        return false;
-    }
-    if(!isLeapYear(4)){
-        return false;
-    }
-    if(isLeapYear(100)){
-        return false;
-    }
-    if(!isLeapYear(400)){
-        return false;
-    }
-    std :: cout << "All test were successful" << std :: endl;
-    return true;
-}
-
